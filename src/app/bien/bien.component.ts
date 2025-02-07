69a2d297efbe8638e2054f6e92bd31bda210399f
aichasakho <aichasakho2205@gmail.com>
2025-02-07 17:23:33 +0000

    suite accueil

--- src/app/bien/bien.component.ts
+++ src/app/bien/bien.component.ts
@@ -12,6 +12,7 @@ export class BienComponent implements OnInit {
   biensToShow: Bien[] = [];
   itemsPerPage: number = 8;
   currentPage: number = 1;
+  bien: any;
 
   constructor(private biensService: BiensService) {}
 
@@ -23,7 +24,7 @@ export class BienComponent implements OnInit {
     this.biensService.getBiens().subscribe(
       (data: Bien[]) => {
         this.biens = data;
-        this.biensToShow = this.biens.slice(0, this.itemsPerPage); 
+        this.biensToShow = this.biens.slice(0, this.itemsPerPage);
       },
       (error) => {
         console.error('Erreur lors du chargement des biens', error);
@@ -35,8 +36,21 @@ export class BienComponent implements OnInit {
     const nextPage = this.currentPage + 1;
     const startIndex = this.itemsPerPage * nextPage;
     const endIndex = startIndex + this.itemsPerPage;
-    this.biensToShow = this.biens.slice(0, endIndex); 
+    this.biensToShow = this.biens.slice(0, endIndex);
 
     this.currentPage = nextPage;
+  }
+
+  reserverWhatsApp(): void {
+    const message = `Je souhaite réserver le bien: ${this.bien.titre} avec le prix de ${this.bien.prix} FCFA.`;
+    const phoneNumber = '+221776819474';
+    window.open(`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`, '_blank');
+  }
+
+  reserverEmail(): void {
+    const subject = `Réservation pour ${this.bien.titre}`;
+    const body = `Je souhaite réserver le bien suivant :\n\nTitre: ${this.bien.titre}\nPrix: ${this.bien.prix} FCFA\nDescription: ${this.bien.description}`;
+    const email = 'sakhoaichatou11@gmail.com.com';
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
   }
 }
